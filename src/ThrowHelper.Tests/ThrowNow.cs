6be0a557837b103fcb80f3d0877bdcd6aca843ab
elemarjr <elemarjr@gmail.com>
2012-05-27 10:10:47 -0300

    Now!

---
+++ src/ThrowHelper.Tests/ThrowNow.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace ThrowHelper.Tests
+{
+    [TestFixture]
+    public class ThrowNow
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Throw.GlobalModifier = null;
+        }
+
+        [Test]
+        public void Now_PassingNull_ThrowsArgumentNullException()
+        {
+            Executing.This(() => Throw.Now(null))
+                .Should().Throw<ArgumentNullException>()
+                .And.Exception.ParamName.Should().Be("exception");
+        }
+
+        [Test]
+        public void Now_PassingAnException_ThrowsIt()
+        {
+            var exception = new InvalidOperationException();
+            Executing.This(() => Throw.Now(exception))
+                .Should().Throw<InvalidOperationException>();
+        }
+
+        [Test]
+        public void Now_PassingAnExceptionAndAModifier_ThrowsModifiedException()
+        {
+            var exception = new InvalidOperationException();
+            Func<Exception, Exception> modifier = (e) =>
+                        new Exception("Outer Exception", e);
+
+            Executing.This(() => Throw.Now(exception, modifier))
+                .Should().Throw<Exception>()
+                .And.Exception.InnerException.Should().Be(exception);
+        }
+
+        [Test]
+        public void Now_PassingAnExceptionWithGlobalModifier_ThrowsModifiedException()
+        {
+            var exception = new InvalidOperationException();
+            Func<Exception, Exception> modifier = (e) =>
+                        new Exception("Outer Exception", e);
+
+            Throw.GlobalModifier = modifier;
+
+            Executing.This(() => Throw.Now(exception))
+                .Should().Throw<Exception>()
+                .And.Exception.InnerException.Should().Be(exception);
+        }
+    }
+}
---
+++ src/ThrowHelper/Throw.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ThrowHelper
+{
+    public static class Throw
+    {
+        public static Func<Exception, Exception> GlobalModifier { get; set; }
+
+        [DebuggerStepThrough]
+        public static void Now(
+            Exception exception,
+            Func<Exception, Exception> modifier = null
+            )
+        {
+
+            if (exception == null)
+                throw new ArgumentNullException("exception");
+
+            Exception e = exception;
+
+            if (modifier != null)
+                e = modifier(exception) ?? exception;
+
+            if (GlobalModifier != null)
+                e = GlobalModifier(e) ?? exception;
+
+            throw e;
+        }
+    }
+}
